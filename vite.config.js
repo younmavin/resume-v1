76b0e8719cc9ae1bed2f9b3b5368aa1a9828a3ce
younmavin <ddw9819@gmail.com>
2026-06-07 15:34:54 +0900

    base경로 변경 및 빌드

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()], //
-  base: '/resume/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
